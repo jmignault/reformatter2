c642b4e0c2a723950b4a699d6167575b3e563b5d
jmignault <john.mignault@gmail.com>
2023-05-21 11:03:34 -0400

    update folder in place

--- reformatter.py
+++ reformatter.py
@@ -7,34 +7,42 @@ import datetime
 def wp_to_pdf(fn, pdir):
    cmdstr = 'soffice --convert-to pdf --outdir "' + pdir + '" "' + fn + '"'
    os.system(cmdstr)
-  
+
+def tif2pdf(fn, pdir):
+   outf = os.path.join(pdir, os.path.basename(fn) + '.pdf')
+   cmdstr = 'convert -density 120 -quality 10 -compress jpeg ' + fn + ' ' + outf
+   os.system(cmdstr)
+   
 # dictionary of format conversions: keys are extensions, value is function to call for conversion 
-formats = {'.doc':wp_to_pdf, '.docx':wp_to_pdf, '.wp':wp_to_pdf, '.wpd':wp_to_pdf, '.pub':wp_to_pdf, '.txt':wp_to_pdf}
+formats = {'.doc':wp_to_pdf, '.docx':wp_to_pdf, '.wp':wp_to_pdf, '.wpd':wp_to_pdf, '.pub':wp_to_pdf, '.tif':tif2pdf }
 
 # make a list of extensions
 fkeys = list(formats.keys())
+
 # define arguments and parse them
+fld = os.getcwd()
 parser = argparse.ArgumentParser(description='Convert a folder of files to pdf format.')
-parser.add_argument('infiles', help="Folder containing files to be converted")
+parser.add_argument('folder', help="Folder containing files to be converted")
 
-args = parser.parse_args()
+aggies = parser.parse_args()
 
-outpath = os.path.join(os.getcwd(), "processed")
-if not (os.path.exists(outpath)):
-   os.mkdir(outpath)
-
+# set up logfile
+fname = os.getcwd() if aggies.folder == '.' else aggies.folder
+dname = os.path.basename(os.path.normpath(fname))
 tstamp = datetime.date.strftime(datetime.datetime.now(), "%m%d%y%M%S")
-dname = os.path.basename(os.path.normpath(args.infiles))
-logfn = os.path.join(outpath, f"{tstamp}_{dname}_log.txt")
-
+logfn = os.path.join(os.path.normpath(aggies.folder),'..', f"{tstamp}_{dname}_log.txt")
 logf = open(logfn, 'w', encoding="utf-8")
 
-for path, subdir, files in os.walk(args.infiles):
+# figure out how to get the right directory to save files to depending
+# on whether output is in mirror dir structure or in place.
 
-   procdir = os.path.join(outpath, os.path.relpath(path, os.path.dirname(args.infiles)))
-   if not(os.path.exists(procdir)):
-      os.mkdir(procdir)
+for path, subdir, files in os.walk(fname):
 
+    if files:
+       procdir = os.path.join(os.path.normpath(path), '../access')
+       if not(os.path.exists(procdir)):
+          os.mkdir(procdir)
+   
    for name in files:
       # get a full path to pass to function
       fn = os.path.join(path, name)
@@ -59,5 +67,3 @@ for path, subdir, files in os.walk(args.infiles):
          logf.write(f"{lstamp}: Skipped {fn}\n")
 
 logf.close()
-
-
