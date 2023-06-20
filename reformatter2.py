c03d75c351b815289a29a513bdff6de4dfaa5f44
jmignault <john.mignault@gmail.com>
2023-06-20 10:24:16 -0400

    Refactor filename and glob

--- reformatter2.py
+++ reformatter2.py
@@ -2,11 +2,15 @@
 import os
 import argparse
 import datetime
+from glob import glob
+from pathlib import PurePath
 
 # converter functions. Must take 2 arguments: filename and output directory.
 def tif2pdf(fn, pdir):
-   outf = os.path.join(pdir, os.path.basename(fn) + '.pdf')
-   cmdstr = 'convert -density 120 -quality 10 -compress jpeg ' + fn + ' ' + outf
+   # outf = os.path.join(pdir, os.path.basename(fn) + '.pdf')
+   globs = glob(os.path.join(pdir, "*.tif"))
+   globs.sort()
+   cmdstr = f'convert -density 120 -quality 10 -compress jpeg {" ".join(globs)} {fn}'
    os.system(cmdstr)
    
 # dictionary of format conversions: keys are extensions, value is function to call for conversion 
@@ -34,26 +38,28 @@ logf = open(logfn, 'w', encoding="utf-8")
 
 for path, subdir, files in os.walk(fname):
 
-    if files:
-       procdir = os.path.join(os.path.normpath(path), '../access')
-       if not(os.path.exists(procdir)):
-          os.mkdir(procdir)
-   
-    for name in files:
+    if os.path.basename(path) == 'master' and files:
+      procdir = os.path.join(os.path.normpath(path), '../access')
+      if not(os.path.exists(procdir)):
+        os.mkdir(procdir)
+
       # get a full path to pass to function
-      fn = os.path.join(path, name)
+      fn = os.path.join(path, files[0])
       # get the file extension
       ext = os.path.splitext(fn)[-1].lower()
       # calculate a timestamp for the logfile
       lstamp = datetime.date.strftime(datetime.datetime.now(), "%m-%d-%y:%M:%S")
- 
+
       # does file need conversion?
       if ext in fkeys:
          # yes, print a msg & call function associated with the extension
          try:
-            print(f"Converting {fn}")
-            formats[ext](fn, procdir)
-            logstr = f"{lstamp}: Converted {fn} to pdf\n"
+            print(f"Converting {path}")
+            # outf = os.path.join(procdir, os.path.basename(fn) + '.pdf')
+            pdfpath = PurePath(fn)
+            pdffn = os.path.join(procdir, pdfpath.parts[1] + '.pdf')
+            formats[ext](pdffn, path)
+            logstr = f"{lstamp}: Converted {path} to pdf\n"
             logf.write(logstr)
          except BaseException as err:
             logf.write(f"{lstamp}:Could not convert {fn}: {err}")
